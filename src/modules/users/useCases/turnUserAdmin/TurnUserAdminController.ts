013cfbb7bfd7d8a39179c70164b14601379b3de9
jorge21ll <jorge.lllucio@gmail.com>
2022-01-08 20:13:42 -0300

    version 1.0.0

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const id = user_id.toString();
+      const users = this.turnUserAdminUseCase.execute({ user_id: id });
+
+      return response.status(200).json(users);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
